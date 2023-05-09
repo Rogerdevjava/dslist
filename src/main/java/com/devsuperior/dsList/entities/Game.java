d8038f45ca7325ab7e3af2661aba198a084276a7
Rogerio Ribeiro <rogerioribeiro@rdtechfitness.com.br>
2023-05-08 23:06:15 -0300

    Game seed

--- src/main/java/com/devsuperior/dsList/entities/Game.java
+++ src/main/java/com/devsuperior/dsList/entities/Game.java
@@ -19,22 +19,28 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
-	private String ingUrl;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String ingUrl,
-			String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score,
+			String imgUrl, String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
-		this.ingUrl = ingUrl;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -71,20 +77,28 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platform) {
+		this.platforms = platform;
+	}
+	
+	public Double getScore() {
+		return score;
 	}
 
-	public String getIngUrl() {
-		return ingUrl;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
-	public void setIngUrl(String ingUrl) {
-		this.ingUrl = ingUrl;
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
